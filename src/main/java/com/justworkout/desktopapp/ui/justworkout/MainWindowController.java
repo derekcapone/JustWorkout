3c5a07995019ebff2267861f2b3b691ae863a3ed
Derek Capone <derekcapone7@gmail.com>
2023-11-18 23:47:40 -0400

    Adds some basic setup for the main view

---
+++ src/main/java/com/justworkout/desktopapp/ui/justworkout/MainWindowController.java
@@ -0,0 +1,12 @@
+package com.justworkout.desktopapp.ui.justworkout;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainWindowController {
+
+    public void MainWindowController() {
+
+    }
+
+}
